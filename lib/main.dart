9eefeaf40ae432eb8b772fbee6cb8f29bb5d4791
AlkhadafiS <alkhadafi.saddam21@students.unila.ac.id>
2023-10-02 18:44:06 +0700

    tes

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         textTheme: GoogleFonts.poppinsTextTheme(),
       ),
-      home: HomeScreen(),
+      home: OnboardingScreen(),
     );
   }
 }
